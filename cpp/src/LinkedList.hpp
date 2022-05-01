118cf0c13a2e5153774d5e333090adc23579ef64
liminchian <liminchian@gmail.com>
2022-05-01 17:07:53 +0800

    更動LinkedList打印函式

--- cpp/src/LinkedList.hpp
+++ cpp/src/LinkedList.hpp
@@ -10,17 +10,18 @@ struct ListNode {
   ListNode(int x, ...) : val(x), next(nullptr) {}
   ListNode(int x, ListNode *n) : val(x), next(n) {}
 
-public:
-  std::string display() {
-    ListNode *cur = this;
-    std::string msg;
-    while (cur != nullptr) {
-      msg += (std::to_string(cur->val) + "->");
-      cur = cur->next;
+  std::string display_value(std::string s, ListNode *cur) {
+    if (cur == nullptr) {
+      s += "NULL";
+      return s;
     }
-    msg.append("NULL");
-    return msg;
+    s += std::to_string(cur->val);
+    s += "->";
+    return display_value(s, cur->next);
   }
+
+public:
+  std::string display() { return display_value("", this); }
 
   void append(int val) {
     ListNode *cur = this, *node = new ListNode(val);
@@ -28,6 +29,21 @@ public:
       cur = cur->next;
     }
     cur->next = node;
+  }
+
+  void insert(int index, int val) {
+    int idx = 0;
+    ListNode *cur = this, *node = new ListNode(val);
+    while (cur != nullptr) {
+      if (idx == index) {
+        ListNode *temp = cur->next;
+        node->next = temp;
+        cur->next = node;
+        break;
+      }
+      cur = cur->next;
+      idx++;
+    }
   }
 };
 
