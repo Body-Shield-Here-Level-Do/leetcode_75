74f281f4ed5ff3c2b19598701bb71278e36f1a99
liminchian <liminchian@gmail.com>
2022-06-04 10:14:13 +0800

    Revert "第十三次會議記錄準備資料"
    This reverts commit 9c04508006230f8f3f67dad86a1963f9ea64e93c.

--- cpp/src/Tree.hpp
+++ cpp/src/Tree.hpp
@@ -1,6 +1,3 @@
-#include <string>
-#include <vector>
-
 #ifndef TREE_HPP
 #define TREE_HPP
 
@@ -12,10 +9,6 @@ struct TreeNode {
   TreeNode() : val(0), left(nullptr), right(nullptr) {}
   TreeNode(int x, ...) : val(x), left(nullptr), right(nullptr) {}
   TreeNode(int x, TreeNode *l, TreeNode *r) : val(x), left(l), right(r) {}
-
-  std::vector<std::string> dfs();
-  std::vector<std::string> wfs();
-  std::vector<std::string> inorder();
 };
 
 #endif // !TREE_HPP
