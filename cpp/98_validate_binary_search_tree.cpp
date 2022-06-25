ba42ce37197c54847e1bcde1d7b8aef1769ef514
liminchian <liminchian@gmail.com>
2022-06-25 21:02:29 +0800

    Prepare for No.16 Meeting

---
+++ cpp/98_validate_binary_search_tree.cpp
@@ -0,0 +1,34 @@
+#include "src/Tree.hpp"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  vector<int> tree;
+
+  void inorder(TreeNode *root) {
+    if (root == nullptr) {
+      return;
+    }
+    inorder(root->left);
+    tree.push_back(root->val);
+    inorder(root->right);
+  }
+
+  bool isValidBST(TreeNode *root) {
+    if (root == nullptr) {
+      return true;
+    }
+
+    inorder(root);
+
+    for (int i = 1; i < tree.size(); i++) {
+      if (tree[i] <= tree[i - 1]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
